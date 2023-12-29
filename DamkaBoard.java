ee3d0126d6408b94219a6857d300b0d535580dbf
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-30 00:45:30 +0200

    Update DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -2,22 +2,23 @@
  *  Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-	 	int a = Integer.parseInt(args[0]);
-		String b = "";
-		String c = "";
+    public static void main(String[] args) {
+	int a = Integer.parseInt(args[0]);
+	String b = "";
+	String c = "";
 
-		for (int i = 1; i < (a + 1); i++) {
-			b += "* ";
-			c += " *";
-		};
-
-		for (int i = 1; i < (a + 1); i++) {
-			if (i % 2 ==0) { System.out.println(c); }
-			else { System.out.println(b); }
-		}
-
+	for (int i = 1; i < (a + 1); i++) {
+	     b += "* ";
+	     c += " *";
 	}
+	for (int i = 1; i < (a + 1); i++) {
+	     if (i % 2 ==0) {
+	      System.out.println(c);
+	     } else {
+	      System.out.println(b);
+	     }
+	}
+    }
 }
 	
 
