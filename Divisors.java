259e61e55cdedbbd0cad61b1c925a0167921582f
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-29 22:06:38 +0200

    Update Divisors.

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,12 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+	int a = Integer.parseInt(args[0]);
+
+	for (int i = 1; i < (a + 1); i++) {
+		if (a % i == 0) {
+		System.out.println(i);	
+			}
+		}
 	}
 }
