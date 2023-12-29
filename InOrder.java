608f119276099be08c58e2d0288a26805e38cf1c
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-30 00:19:44 +0200

    Update InOrder

--- InOrder.java
+++ InOrder.java
@@ -3,18 +3,19 @@
  *  as long as they form a non-decreasing sequence.
  */
 public class InOrder {
-	public static void main (String[] args) {
-	int random1 = (int) (10 * Math.random());
+    public static void main (String[] args) {
+        int random1 = (int) (10 * Math.random());
 	int random2;
         System.out.print(random1 + " ");
         
-		do {
-			 random2 = (int) (10 * Math.random());
-			if (random2 >= random1){ 
-			 System.out.print(random2 + " ");	
-			 random1= random2;
-		     }
-		} while (random2 >= random1);
-		System.out.println();
-	  } 
-	} 
+	do {
+	    random2 = (int) (10 * Math.random());
+	    if (random2 >= random1) { 
+		System.out.print(random2 + " ");	
+		random1 = random2;
+	    }
+	} while (random2 >= random1);
+	    
+	System.out.println();
+    } 
+} 
