263beb7217d2ccd22d3c703258dcf69937bfb16b
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-29 22:09:50 +0200

    Update Perfect

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,17 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int a = Integer.parseInt(args[0]);
+		int sum = 1;
+		String b = "";
+
+		for (int i = 2; i < a; i++) {
+			if (a % i == 0) {
+			b += (" + " + i);
+			sum += i;
+		}}
+		   if (sum == a) {System.out.println(a + " is a perfect number since " + a + " = 1" + b);
+		}
+		   else {System.out.println(a + " is not a perfect number");}
+		}
 	}
-}
