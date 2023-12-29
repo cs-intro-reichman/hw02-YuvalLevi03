1704708684918c7b38ca83ae491312d6cca1df91
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-30 00:00:02 +0200

    Update Reverse

--- Reverse.java
+++ Reverse.java
@@ -3,15 +3,15 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-	String s = args[0];
-	int length = s.length();
+    public static void main (String[] args){
+    String s = args[0];
+    int length = s.length();
 
-	for (int i = (length - 1); i >= 0 ; i--) {
-		System.out.print(s.charAt(i));
-	 };
-	    System.out.println();
-	    System.out.println("The middle character is " + 
-	    	                s.charAt(((length + 1) / 2) - 1));
-	}
-}
+    for (int i = (length - 1); i >= 0 ; i--) {
+	System.out.print(s.charAt(i));
+    }
+        System.out.println();
+        System.out.println("The middle character is " + 
+	    	           s.charAt(((length + 1) / 2) - 1));
+    }
+ }
