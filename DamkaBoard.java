ea8c64813961ae7dbe384b6ace5499db6d584761
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-29 22:05:04 +0200

    Update DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,21 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+	 	int a = Integer.parseInt(args[0]);
+		String b = "";
+		String c = "";
+
+		for (int i = 1; i < (a + 1); i++) {
+			b += "* ";
+			c += " *";
+		};
+
+		for (int i = 1; i < (a + 1); i++) {
+			if (i % 2 ==0) { System.out.println(c); }
+			else { System.out.println(b); }
+		}
+
 	}
 }
+	
+
