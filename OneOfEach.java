69ef1574306f89e3ea95c9a379066e9aec4dc745
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-30 00:53:05 +0200

    Update OneOfEach

--- OneOfEach.java
+++ OneOfEach.java
@@ -4,31 +4,31 @@
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
-	public static void main (String[] args) {
-		boolean isGirl = false;
-		boolean isBoy = false;
-		double random;
-		int sum = 0;
+    public static void main (String[] args) {
+	boolean isGirl = false;
+	boolean isBoy = false;
+	double random;
+	int sum = 0;
 
-		do { 
-            random = Math.random();
-            if (random < 0.5) {
-            	isBoy = true;
-            	sum += 1;
-            	System.out.print("b");
-            }
-            else {
-            	isGirl = true;
-            	sum += 1;
-            	System.out.print("g");
-            } 
+	do { 
+           random = Math.random();
+           if (random < 0.5) {
+              isBoy = true;
+              sum += 1;
+              System.out.print("b");
+           }
+           else {
+              isGirl = true;
+              sum += 1;
+              System.out.print("g");
+           } 
            if (! (isGirl && isBoy)) {
-        	    System.out.print(" ");
-            }
-     	}
-        while (! (isGirl && isBoy));
+              System.out.print(" ");
+           }
+     	} while (! (isGirl && isBoy));
 
-            System.out.println();
-            System.out.println("You made it... and you now have " + sum + " children.");
-	}
-}
+        System.out.println();
+        System.out.println("You made it... and you now have " +
+			   sum + " children.");
+    }
+ }
