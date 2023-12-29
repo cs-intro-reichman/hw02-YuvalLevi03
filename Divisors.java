2e0d8103729ce151d9b6cf44e78012509cc27293
YuvalLevi03 <154444159+YuvalLevi03@users.noreply.github.com>
2023-12-29 23:50:00 +0200

    Update Divisors

--- Divisors.java
+++ Divisors.java
@@ -2,13 +2,13 @@
  *  Gets a command-line argument (int), and prints all the divisors of the given number.
  */
 public class Divisors {
-	public static void main (String[] args) {
-	int a = Integer.parseInt(args[0]);
+    public static void main (String[] args) {
+        int a = Integer.parseInt(args[0]);
 
-	for (int i = 1; i < (a + 1); i++) {
-		if (a % i == 0) {
-		System.out.println(i);	
-			}
-		}
-	}
+        for (int i = 1; i < (a + 1); i++) {
+	    if (a % i == 0) {
+	    System.out.println(i);	
+	    }
+        }
+    }
 }
